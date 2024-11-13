8843dd1bdb4e538ee635820b93c96877b6c6fd35
c_ringwald <42188996+datalogism@users.noreply.github.com>
2024-11-13 17:32:08 +0100

    Add files via upload

---
+++ kstor/initializeKastorFromShape.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 27 10:35:40 2024
+
+@author: cringwal
+"""
+
+from argparse import ArgumentParser
+import logging
+
+from rdflib import Graph
+import src.class_signatures as cs
+import src.triple_shapes as ts
+
+if __name__ == '__main__':
+    parser = ArgumentParser()
+    parser.add_argument("-s", "--shape_file_path", default=None)
+    args = parser.parse_args()
+
+    sparql_ep = 'http://localhost:8080/sparql'
+    if args.shape_file_path:
+        shape = Graph()
+        shape.parse(args.shape_file_path)
+        type_triples = ts.getShapeType(shape)
+        logging.info("START uuid creation process")
+        res = cs.CreateUUIDClassEntities(type_triples, sparql_ep)
+        logging.info("END uuid creation process")
+    else:
+        logging.error("Shape file path not provided")
