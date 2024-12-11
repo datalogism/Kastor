b0de9dbf14d3c64ea2b1730ed2cb8b95c69e0b57
c_ringwald <42188996+datalogism@users.noreply.github.com>
2024-12-11 15:35:42 +0100

    Add files via upload

---
+++ slm/job_bart_test_init.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#
+#### JOB INFO
+#SBATCH --job-name=$JOB_NAME$
+#SBATCH --output=$LOGS_OUPUT_FILE$
+###### CONF
+#SBATCH --ntasks=1
+#SBATCH --nodes=1
+#SBATCH --time=24:00:00
+#SBATCH --account=$YOURACCOUNT$
+####### CONTACT
+#SBATCH --mail-user=$YOURMAIL$
+#SBATCH --mail-type=BEGIN,END,FAIL
+######### GPU
+#SBATCH --gres=gpu:1
+#SBATCH --partition=gpu
+
+module purge
+module load miniconda
+conda activate env_shapes
+
+export TOKENIZERS_PARALLELISM=True
+export WANDB_API_KEY="YOURAPIKEY"
+python ./src/test_DS.py model=bart_base_model data=YOURDATACONFIGFILE train=bart_dbpedia
